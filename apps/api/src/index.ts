1f3f02ce2c7ef6af584a2299f589d0f61614687f
Abishek Balaji <abishekbalaji7.151@gmail.com>
2026-06-09 19:13:56 +0530

    feat: add express api with health endpoint

---
+++ apps/api/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+app.get("/health", (_req, res) => {
+  res.status(200).json({
+    status: "ok",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`To Do App is listening on port ${port}`);
+});
