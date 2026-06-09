ea33a650fb76e98300e3770826351fc639e1915d
Abishek Balaji <abishekbalaji7.151@gmail.com>
2026-06-09 21:56:30 +0530

    feat: serve tasks from postgres via pg

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,24 @@
+import "dotenv/config";
 import express from "express";
+import { Pool } from "pg";
 import type { Task } from "@todo-app/shared";
 
 const app = express();
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({
+  connectionString,
+});
+
+type DBTask = {
+  id: number;
+  title: string;
+  completed: boolean;
+  created_at: Date;
+  notes: string;
+};
 
 app.get("/health", (_req, res) => {
   res.status(200).json({
@@ -10,26 +26,32 @@ app.get("/health", (_req, res) => {
   });
 });
 
-app.get("/tasks", (_req, res) => {
-  const tasks: Task[] = [
-    {
-      id: "1",
-      title: "task1",
-      completed: true,
-      createdAt: "20/1/2026",
-      notes: "",
-    },
-    {
-      id: "2",
-      title: "task2",
-      completed: true,
-      createdAt: "20/1/2026",
-      notes: "",
-    },
-  ];
-  res.status(200).json({
-    tasks,
-  });
+app.get("/tasks", async (_req, res) => {
+  try {
+    const result = (
+      await pool.query<DBTask>(
+        "select id, title, completed, created_at, notes from tasks",
+      )
+    ).rows;
+    const tasks: Task[] = result.map(
+      (task: DBTask): Task => ({
+        id: task.id,
+        title: task.title,
+        completed: task.completed,
+        createdAt: task.created_at.toISOString(),
+        notes: task.notes,
+      }),
+    );
+
+    res.status(200).json({
+      tasks,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({
+      error: "Internal Server error",
+    });
+  }
 });
 
 app.listen(port, () => {
